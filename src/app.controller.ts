81a5ac457b41f38d059957e7b220f363fba06b99
Kitiwat Daowrueng <64160131@go.buu.ac.th>
2024-09-22 16:19:02 +0700

    Finish Inventory

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 import {
+  ClientProxy,
   Ctx,
   MessagePattern,
   Payload,
@@ -9,7 +10,10 @@ import {
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('Order_SERVICE') private orderService: ClientProxy,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -28,9 +32,13 @@ export class AppController {
     if (isInstock) {
       console.log('Inventory available. Processing Order.');
       channel.ack(originalMsg);
+      // Completed Order
+      this.orderService.emit('order_completed', data);
     } else {
       console.log('Inventory non available.');
       channel.ack(originalMsg);
+      // Canceled Order
+      this.orderService.emit('order_canceled', data);
     }
   }
 }
