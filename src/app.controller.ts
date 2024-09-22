1cd53e7615301106f170b6571dc9c219108d31c3
Kitiwat Daowrueng <64160131@go.buu.ac.th>
2024-09-22 15:56:19 +0700

    add handleOrderCreated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +14,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('order_created')
+  handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    // console.log(`Pattern: ${context.getPattern()}`);
+    // console.log(context.getMessage());
+    // console.log(context.getChannelRef());
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    console.log('Order received for processing', data);
+    const isInstock = false;
+    if (isInstock) {
+      console.log('Inventory available. Processing Order.');
+      channel.ack(originalMsg);
+    } else {
+      console.log('Inventory non available.');
+      channel.ack(originalMsg);
+    }
   }
 }
